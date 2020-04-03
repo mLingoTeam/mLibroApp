0ecd1bb94e13d6858f9797316a24336246c74435
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-04-03 22:23:57 +0200

    state formular works

--- mlibro/src/pages/Formular.js
+++ mlibro/src/pages/Formular.js
@@ -7,9 +7,16 @@ class Formular extends React.Component{
             "username":"",
             "password": ""
         }
+
+        this.handleChange = this.handleChange.bind(this);
     }
 
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+      }
+
     render(){
+        console.log(this.state)
         return(
             <div className="formular--container col m12">
                 <div>
@@ -21,13 +28,13 @@ class Formular extends React.Component{
                         <form className="col s12 m12">
                             <div className="row">
                                 <div className="input-field col s12">
-                                <input id="username" type="text" className="validate"/>
+                                <input id="username" type="text" className="validate" name="username" value={this.state.username} onChange={this.handleChange}/>
                                 <label for="username">Username</label>
                             </div>
                         </div>
                         <div className="row">
                             <div className="input-field col s12">
-                                <input id="password" type="password" className="validate"/>
+                                <input id="password" type="password" className="validate" name="password" value={this.state.password} onChange={this.handleChange}/>
                                 <label for="password">Password</label>
                             </div>
                         </div>
