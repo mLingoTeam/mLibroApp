1c81d21f9e208b833fb1873af1840db67b966db7
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-04-03 21:36:08 +0200

    Add back button

--- mlibro/src/components/Task.js
+++ mlibro/src/components/Task.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import {MdChevronLeft} from 'react-icons/md'
+import {Link} from 'react-router-dom'
 const Task = ({props}) => {
 
     const {przedmiot, tytul, tresc, data_zadania, termin_oddania, id} = props;
 
     return(
         <div className="task--card">
-            <ul class="collection with-header">
-                <li class="collection-header"><h5>{przedmiot}</h5></li>
-                <li class="collection-item">{tytul}</li>
-                <li class="collection-item">{tresc}</li>
-                <li class="collection-item">Od: {data_zadania}</li>
-                <li class="collection-item">Do: {termin_oddania}</li>
+            <ul className="collection with-header">
+                <li className="collection-header"><h5>{przedmiot}</h5></li>
+                <li className="collection-item">{tytul}</li>
+                <li className="collection-item">{tresc}</li>
+                <li className="collection-item">Od: {data_zadania}</li>
+                <li className="collection-item">Do: {termin_oddania}</li>
             </ul>
+            <Link className="button-mlibro" to="/"><MdChevronLeft/></Link>
         </div>
     )
 }
