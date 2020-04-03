48dda7393b0a8f3efd0fde2d5fc1ca69f1da9759
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-04-03 20:54:03 +0200

    change navbar

--- mlibro/src/components/Header.js
+++ mlibro/src/components/Header.js
@@ -6,22 +6,20 @@ const Header = () => {
         <div className="header">
                 <nav className="navbar-fixed">
                     <div className="nav-wrapper container">
-                    <a href="#!" className="brand-logo">Logo</a>
+                    <a href="#!" className="brand-logo">mLibro</a>
                     <a href="#" data-target="mobile-demo" className="sidenav-trigger"><FaHamburger/></a>
                     <ul className="right hide-on-med-and-down">
-                        <li><a href="sass.html">Sass</a></li>
-                        <li><a href="badges.html">Components</a></li>
-                        <li><a href="collapsible.html">Javascript</a></li>
-                        <li><a href="mobile.html">Mobile</a></li>
+                        <li><a href="sass.html">Today</a></li>
+                        <li><a href="badges.html">All</a></li>
+                        <li><a href="collapsible.html">mLingo Team</a></li>
                     </ul>
                     </div>
                 </nav>
 
                 <ul className="sidenav" id="mobile-demo">
-                    <li><a href="sass.html">Sass</a></li>
-                    <li><a href="badges.html">Components</a></li>
-                    <li><a href="collapsible.html">Javascript</a></li>
-                    <li><a href="mobile.html">Mobile</a></li>
+                        <li><a href="sass.html">Today</a></li>
+                        <li><a href="badges.html">All</a></li>
+                        <li><a href="collapsible.html">mLingo Team</a></li>
                 </ul>
         </div>
     )
